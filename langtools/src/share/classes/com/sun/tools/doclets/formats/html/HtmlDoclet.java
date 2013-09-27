a817b6312922176eb24749039f83dc24f975a77d
Alexander Zuev <kizune@openjdk.org>
2013-09-27 21:20:01 +0400

    6978886: javadoc shows stacktrace after print error resulting from disk full
    Reviewed-by: jjg

--- langtools/src/share/classes/com/sun/tools/doclets/formats/html/HtmlDoclet.java
+++ langtools/src/share/classes/com/sun/tools/doclets/formats/html/HtmlDoclet.java
@@ -185,6 +185,10 @@ public class HtmlDoclet extends AbstractDoclet {
                             .getClassBuilder(curr, prev, next, classtree);
                     classBuilder.build();
                 }
+            } catch (IOException e) {
+                throw new DocletAbortException(e);
+            } catch (DocletAbortException de) {
+                throw de;
             } catch (Exception e) {
                 e.printStackTrace();
                 throw new DocletAbortException(e);
