8809f1266fc02c62bb17f266f52d756b6f7a7f0e
Mikhail Cherkasov <mcherkas@openjdk.org>
2017-01-18 02:02:14 +0000

    8037099: [macosx] Remove all references to GC from native OBJ-C code
    Reviewed-by: serb

--- jdk/src/macosx/native/sun/awt/CClipboard.m
+++ jdk/src/macosx/native/sun/awt/CClipboard.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -73,7 +73,6 @@ static CClipboard *sClipboard = nil;
 
     [super dealloc];
 }
-//- (void)finalize { [super finalize]; }
 
 - (NSData *)data {
     return fData;
