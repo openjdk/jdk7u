978427b57aca59d49a65b87dd1120ecd049303c2
Alexander Scherbatiy <alexsch@openjdk.org>
2017-04-20 20:28:44 +0100

    8011059: [macosx] Support automatic @2x images loading on Mac OS X
    Reviewed-by: serb, flar

--- jdk/src/share/classes/sun/awt/SunHints.java
+++ jdk/src/share/classes/sun/awt/SunHints.java
@@ -171,7 +171,7 @@ public class SunHints {
         }
     }
 
-    private static final int NUM_KEYS = 9;
+    private static final int NUM_KEYS = 10;
     private static final int VALS_PER_KEY = 8;
 
     /**
@@ -251,6 +251,13 @@ public class SunHints {
     public static final int INTVAL_STROKE_NORMALIZE = 1;
     public static final int INTVAL_STROKE_PURE = 2;
 
+    /**
+     * Image scaling hint key and values
+     */
+    public static final int INTKEY_RESOLUTION_VARIANT = 9;
+    public static final int INTVAL_RESOLUTION_VARIANT_DEFAULT = 0;
+    public static final int INTVAL_RESOLUTION_VARIANT_OFF = 1;
+    public static final int INTVAL_RESOLUTION_VARIANT_ON = 2;
     /**
      * LCD text contrast control hint key.
      * Value is "100" to make discontiguous with the others which
@@ -449,6 +456,24 @@ public class SunHints {
                            SunHints.INTVAL_STROKE_PURE,
                            "Pure stroke conversion for accurate paths");
 
+    /**
+     * Image resolution variant hint key and value objects
+     */
+    public static final Key KEY_RESOLUTION_VARIANT =
+        new SunHints.Key(SunHints.INTKEY_RESOLUTION_VARIANT,
+                         "Global image resolution variant key");
+    public static final Object VALUE_RESOLUTION_VARIANT_DEFAULT =
+        new SunHints.Value(KEY_RESOLUTION_VARIANT,
+                           SunHints.INTVAL_RESOLUTION_VARIANT_DEFAULT,
+                           "Choose image resolutions based on a default heuristic");
+    public static final Object VALUE_RESOLUTION_VARIANT_OFF =
+        new SunHints.Value(KEY_RESOLUTION_VARIANT,
+                           SunHints.INTVAL_RESOLUTION_VARIANT_OFF,
+                           "Use only the standard resolution of an image");
+    public static final Object VALUE_RESOLUTION_VARIANT_ON =
+        new SunHints.Value(KEY_RESOLUTION_VARIANT,
+                           SunHints.INTVAL_RESOLUTION_VARIANT_ON,
+                           "Always use resolution-specific variants of images");
 
     public static class LCDContrastKey extends Key {
 
