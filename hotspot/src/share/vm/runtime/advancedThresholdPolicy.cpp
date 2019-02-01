665610b7cab14ef9099b7aa5eca634c505c064ee
Andrew Haley <aph@openjdk.org>
2019-02-01 10:47:30 +0100

    8145096: Undefined behaviour in HotSpot
    Fix some integer overflows
    
    Reviewed-by: jrose, kvn, kbarrett, adinn, iklam, dcherepanov

--- hotspot/src/share/vm/runtime/advancedThresholdPolicy.cpp
+++ hotspot/src/share/vm/runtime/advancedThresholdPolicy.cpp
@@ -125,7 +125,8 @@ bool AdvancedThresholdPolicy::is_old(methodOop method) {
 }
 
 double AdvancedThresholdPolicy::weight(methodOop method) {
-  return (method->rate() + 1) * ((method->invocation_count() + 1) *  (method->backedge_count() + 1));
+  return (double)(method->rate() + 1) *
+    (method->invocation_count() + 1) * (method->backedge_count() + 1);
 }
 
 // Apply heuristics and return true if x should be compiled before y
